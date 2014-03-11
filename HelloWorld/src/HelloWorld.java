144a9b37e6f67fc561b75826c0d80b703c3e9ee4
Manoj_Potluri <Manoj_Potluri>
2014-03-10 20:45:51 -0500

    First change.

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hello GIT");
 		for (int i = 0; i < 2; i++) {
 	    System.out.println(getValue(101));
 		}
