bdab71638bb4498a6015da0eb3a7c191b15de0a1
Manoj_Potluri <Manoj_Potluri@WN7X64-CZRD3Q1.ptc.amer.dell.com>
2014-03-11 16:43:59 -0500

    adding welcome line.

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello GIT");
+		System.out.println("welcome");
 		for (int i = 0; i < 2; i++) {
 	    System.out.println(getValue(101));
 		}
