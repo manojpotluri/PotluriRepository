31119838257e27c500c80682457ea736baac97d6
root <root@manoj>
2014-03-17 21:15:26 -0500

    Changed indentation in Palindrome.java

--- PalindromeTest/src/main/java/org/manojpotluri/PalindromeTest/Palindrome.java
+++ PalindromeTest/src/main/java/org/manojpotluri/PalindromeTest/Palindrome.java
@@ -3,33 +3,24 @@ package org.manojpotluri.PalindromeTest;
 public class Palindrome {
 
 	private String str;
+
 	
-	
-	
+
 	public String getStr() {
 		return str;
 	}
-
-
 
 	public void setStr(String str) {
 		this.str = str;
 	}
 
-
-
 	public boolean isPalindrome(String str) {
 		String s = new StringBuilder(str).reverse().toString();
 		int i = s.compareToIgnoreCase(str);
 		if(i==0)
-		return true;
+			return true;
 		else
-		return false;
+			return false;
 	}
-
-	//public static void main(String[] args) {
-		
-
-	//}
 
 }
